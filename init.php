a9f316256a5ec45c84e3ee92502648849e6cc6ff
benjennings <benjennings@users.noreply.github.com>
2015-04-25 15:29:17 +0100

    Update init.php
    Switched to the newer preference style from https://github.com/mbirth/ttrss_plugin-af_feedmod enabling comma or newline delineated urls

--- init.php
+++ init.php
@@ -1,15 +1,13 @@
 <?php
 
 //
-// More info about this plugin in: http://protagonist.co.uk/markshort
-// Preferences pane from: https://github.com/mbirth/ttrss_plugin-af_feedmod
-// Other inspiration from: https://github.com/atallo/ttrss_fullpost
-
+// More info about this plugin in: http://protagonist.co.uk
+//
 class af_MarkShort extends Plugin implements IHandler{
     private $host;
 
     function about() {
-        return array(0.01,
+        return array(0.02,
             "MarkShort. Appends a [short] marker to the end of a title when a short article is detected.",
             "benjennings");
     }
@@ -28,15 +26,19 @@ class af_MarkShort extends Plugin implements IHandler{
     function hook_article_filter($article) {
         $json_conf = $this->host->get($this, 'json_conf');
         $owner_uid = $article['owner_uid'];
-        $data = json_decode($json_conf, true);
 
+        $data = explode(',', str_replace("\n", ",", $json_conf));
+        // trim each element of the array, and then remove empty array elements
+        $data = array_filter(array_map('trim', $data));
         if (!is_array($data)) {
-            // no valid JSON or no configuration at all
+            // no valid configuration, or no configuration at all
             return $article;
         }
 
         foreach ($data as $urlpart) {
+            // file_put_contents('debug_markshort.txt', $article['link'] . " ::test:: ".$urlpart."\n",FILE_APPEND);
             if (stripos($article['link'], $urlpart) === false) continue; // skip this entry, if the URL doesn't match
+            // file_put_contents('debug_markshort.txt', $article['link'] . " ::match:: ". $urlpart."\n",FILE_APPEND);
             if (strpos($article['plugin_data'], "markshort,$owner_uid:") !== false) {
                 // do not process an article more than once
                 if (isset($article['stored']['content'])) $article['content'] = $article['stored']['content'];
@@ -54,9 +56,11 @@ class af_MarkShort extends Plugin implements IHandler{
         return $article;
     }
 
+
     private function get_article_length($content) {
+        // require(__DIR__ . "/html2text.php");
         include_once 'html2text.php';
-        $threshhold = 500; // this is the minimum article content size, change if you like
+        $threshhold = 500; // this is the minimum article content size
 
         $content_length = convert_html_to_text($content);
         if (str_word_count($content_length, 0) < $threshhold) {
@@ -70,80 +74,73 @@ class af_MarkShort extends Plugin implements IHandler{
 //
 // This section deals with the UI for saving of feed preferences
 //
-function hook_prefs_tabs($args)
-{
-    print '<div id="markshortConfigTab" dojoType="dijit.layout.ContentPane"
-                href="backend.php?op=af_markshort"
-                title="' . __('MarkShort') . '"></div>';
-}
+    function hook_prefs_tabs($args)
+    {
+        print '<div id="markshortConfigTab" dojoType="dijit.layout.ContentPane"
+                    href="backend.php?op=af_markshort"
+                    title="' . __('MarkShort') . '"></div>';
+    }
 
-function index()
-{
-    $pluginhost = PluginHost::getInstance();
-    $json_conf = $pluginhost->get($this, 'json_conf');
+    function index()
+    {
+        $pluginhost = PluginHost::getInstance();
+        $json_conf = $pluginhost->get($this, 'json_conf');
 
-    print "<form dojoType=\"dijit.form.Form\">";
+        print "<p>Newline or comma delineated list of URLs to MarkShort.</p>";
+        print "<form dojoType=\"dijit.form.Form\">";
 
-    print "<script type=\"dojo/method\" event=\"onSubmit\" args=\"evt\">
-        evt.preventDefault();
-        if (this.validate()) {
-            new Ajax.Request('backend.php', {
-                parameters: dojo.objectToQuery(this.getValues()),
-                onComplete: function(transport) {
-                    if (transport.responseText.indexOf('error')>=0) notify_error(transport.responseText);
-                    else notify_info(transport.responseText);
-                }
-            });
-            //this.reset();
+        print "<script type=\"dojo/method\" event=\"onSubmit\" args=\"evt\">
+            evt.preventDefault();
+            if (this.validate()) {
+                new Ajax.Request('backend.php', {
+                    parameters: dojo.objectToQuery(this.getValues()),
+                    onComplete: function(transport) {
+                        if (transport.responseText.indexOf('error')>=0) notify_error(transport.responseText);
+                        else notify_info(transport.responseText);
+                    }
+                });
+                //this.reset();
+            }
+            </script>";
+
+        print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"op\" value=\"pluginhandler\">";
+        print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"method\" value=\"save\">";
+        print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"plugin\" value=\"af_markshort\">";
+
+        print "<table width='100%'><tr><td>";
+        print "<textarea dojoType=\"dijit.form.SimpleTextarea\" name=\"json_conf\" style=\"font-size: 12px; width: 99%; height: 500px;\">$json_conf</textarea>";
+        print "</td></tr></table>";
+
+        print "<p><button dojoType=\"dijit.form.Button\" type=\"submit\">".__("Save")."</button>";
+
+        print "</form>";
+    }
+
+    function save()
+    {
+        $json_conf = $_POST['json_conf'];
+        $this->host->set($this, 'json_conf', $json_conf);
+        echo __("Configuration saved.");
+    }
+
+    function csrf_ignore($method)
+    {
+        $csrf_ignored = array("index", "edit");
+        return array_search($method, $csrf_ignored) !== false;
+    }
+
+    function before($method)
+    {
+        if ($_SESSION["uid"]) {
+            return true;
         }
-        </script>";
-
-    print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"op\" value=\"pluginhandler\">";
-    print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"method\" value=\"save\">";
-    print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"plugin\" value=\"af_markshort\">";
-
-    print "<table width='100%'><tr><td>";
-    print "<textarea dojoType=\"dijit.form.SimpleTextarea\" name=\"json_conf\" style=\"font-size: 12px; width: 99%; height: 500px;\">$json_conf</textarea>";
-    print "</td></tr></table>";
-
-    print "<p><button dojoType=\"dijit.form.Button\" type=\"submit\">".__("Save")."</button>";
-
-    print "</form>";
-}
-
-function save()
-{
-    $json_conf = $_POST['json_conf'];
-
-    if (is_null(json_decode($json_conf))) {
-        echo __("error: Invalid JSON!");
         return false;
     }
 
-    $this->host->set($this, 'json_conf', $json_conf);
-    echo __("Configuration saved.");
-}
-
-function csrf_ignore($method)
-{
-    $csrf_ignored = array("index", "edit");
-    return array_search($method, $csrf_ignored) !== false;
-}
-
-function before($method)
-{
-    if ($_SESSION["uid"]) {
+    function after()
+    {
         return true;
     }
-    return false;
-}
-
-function after()
-{
-    return true;
-}
-//
-
 
 }
 ?>
